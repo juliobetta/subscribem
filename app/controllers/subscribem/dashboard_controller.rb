df916b2d34d3c6e377d75c93f64de34ad696b427
Julio Betta <juliobetta@gmail.com>
2014-03-13 02:11:21 -0300

    write first feature: account creation

---
+++ app/controllers/subscribem/dashboard_controller.rb
@@ -0,0 +1,8 @@
+require_dependency "subscribem/application_controller"
+
+module Subscribem
+  class DashboardController < ApplicationController
+    def index
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[File.dir((__FILE__) + "/support/**/*.rb")].each { |f| require f }
+Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f}
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
